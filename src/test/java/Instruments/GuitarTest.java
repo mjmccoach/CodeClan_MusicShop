d2024178044c1e63ba5bd424ce648cbafa9746d0
mjmccoach <mmccoach88@gmail.com>
2020-11-27 18:41:19 +0000

    Tested all setters for instrument and guitar classes

--- src/test/java/Instruments/GuitarTest.java
+++ src/test/java/Instruments/GuitarTest.java
@@ -47,5 +47,46 @@ public class GuitarTest {
     public void canGetGuitarType() {
         assertEquals(GuitarType.ACOUSTIC, guitar.getGuitarType());
     }
-    
+
+    @Test
+    public void canSetMake() {
+        guitar.setMake("Les Paul");
+        assertEquals("Les Paul", guitar.getMake());
+    }
+
+    @Test
+    public void canSetColour(){
+        guitar.setColour("Pink");
+        assertEquals("Pink", guitar.getColour());
+    }
+
+    @Test
+    public void canSetInstrumentType() {
+        guitar.setInstrumentType(InstrumentType.DRUMS);
+        assertEquals(InstrumentType.DRUMS, guitar.getInstrumentType());
+    }
+
+    @Test
+    public void canSetWhole() {
+        guitar.setWholesale(150.00);
+        assertEquals(150.00, guitar.getWholesale(), 0.01);
+    }
+
+    @Test
+    public void canSetRetail() {
+        guitar.setRetail(250.00);
+        assertEquals(250.00, guitar.getRetail(), 0.01);
+    }
+
+    @Test
+    public void canSetStrings() {
+        guitar.setStrings(4);
+        assertEquals(4, guitar.getStrings());
+    }
+
+    @Test
+    public void canSetGuitarType() {
+        guitar.setGuitarType(GuitarType.ELECTRIC);
+        assertEquals(GuitarType.ELECTRIC, guitar.getGuitarType());
+    }
 }
